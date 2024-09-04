728afe02cc0cb22ab223446d42cbd78373da5c31
justkira <shighgamerhg@gmail.com>
2024-09-04 13:02:20 +0300

    chore:  add watermelon db config

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,39 @@
+import { Database } from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
 import { Stack, Link } from 'expo-router';
+import { Platform } from 'react-native';
 
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import migrations from '~/lib/model/migrations';
+import schema from '~/lib/model/schema';
+
+// import Post from './model/Post' // ⬅️ You'll import your Models here
+
+// First, create the adapter to the underlying database:
+const adapter = new SQLiteAdapter({
+  schema,
+  // (You might want to comment it out for development purposes -- see Migrations documentation)
+  migrations,
+  // (optional database name or file system path)
+  // dbName: 'myapp',
+  // (recommended option, should work flawlessly out of the box on iOS. On Android,
+  // additional installation steps have to be taken - disable if you run into issues...)
+  jsi: false /* Platform.OS === 'ios' */,
+  // (optional, but you should implement this method)
+  onSetUpError: (error) => {
+    // Database failed to load -- offer the user to reload the app or log out
+  },
+});
+
+// Then, make a Watermelon database from it!
+const database = new Database({
+  adapter,
+  modelClasses: [
+    // Post, // ⬅️ You'll add Models to Watermelon here
+  ],
+});
 
 export default function Home() {
   return (
